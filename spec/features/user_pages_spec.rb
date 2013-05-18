a8eebc8820672c5e2645f7e84f9499415e53635c
Ivan <ivan@cs-cart.com>
2013-05-18 13:00:05 +0400

    [~] Register user smoke tests has been added.

--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 require 'spec_helper'
 
 Capybara.default_driver = :selenium
@@ -8,29 +10,43 @@ describe "User pages" do
 
   describe "signup" do
 
-    before { visit('http://nastachku.ru/users/new') }
-
-    let(:submit) { "nnn" }
+    before(:each) { visit('http://nastachku.ru/users/new') }
 
     describe "with invalid information" do
       it "should not create a user" do
-        #expect { click_button submit }.not_to change(User, :count)
-	click_button submit
-	expect false
+        find(:xpath, ".//input[@name='commit']").click
+    
+        should have_selector(:xpath, ".//div[contains(@class,'user_email') and contains(@class,'error')]")
+
       end
     end
 
+        time = Time.now.to_i
+        test_email = "test" + time.to_s + "@example.com"
+
     describe "with valid information" do
-      before do
-        fill_in "Name",         with: "Example User"
-        fill_in "Email",        with: "user@example.com"
-        fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
+      before(:each) do
+        fill_in "user[first_name]",             with: "Василий"
+        fill_in "user[last_name]",              with: "Пупкин"
+        fill_in "user[email]",                  with: test_email
+        fill_in "user[password]",               with: "foobar"
+        fill_in "user[password_confirmation]",  with: "foobar"
+        fill_in "user[city]",                   with: "Ульяновск"
       end
 
       it "should create a user" do
-        expect { click_button submit }.to change(User, :count).by(1)
+        find(:xpath, ".//input[@name='commit']").click
+        
+        #FIXME: пока костыль... после исправления бага нужно переделать.
+        should have_content("500 Internal Server Error")
       end
+
+      it "should not create a user with the same email" do
+        find(:xpath, ".//input[@name='commit']").click
+
+        should have_selector(:xpath, ".//div[contains(@class,'user_email') and contains(@class,'error')]")
+      end
+
     end
   end
 end
