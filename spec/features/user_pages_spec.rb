652e58acc8d50ccf29aac05acf27617c8703aafe
Ivan <ivan@cs-cart.com>
2013-05-18 15:59:02 +0400

    [~] Register user tests has been complited.

--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -12,11 +12,16 @@ describe "User pages" do
 
     before(:each) { visit('http://nastachku.ru/users/new') }
 
-    describe "with invalid information" do
+    describe "with empty information" do
       it "should not create a user" do
         find(:xpath, ".//input[@name='commit']").click
     
         should have_selector(:xpath, ".//div[contains(@class,'user_email') and contains(@class,'error')]")
+        should have_selector(:xpath, ".//div[contains(@class,'user_password') and contains(@class,'error')]")
+        should have_selector(:xpath, ".//div[contains(@class,'user_password_confirmation') and contains(@class,'error')]")
+        should have_selector(:xpath, ".//div[contains(@class,'user_first_name') and contains(@class,'error')]")
+        should have_selector(:xpath, ".//div[contains(@class,'user_last_name') and contains(@class,'error')]")
+        should have_selector(:xpath, ".//div[contains(@class,'user_city') and contains(@class,'error')]")
 
       end
     end
@@ -35,6 +40,7 @@ describe "User pages" do
       end
 
       it "should create a user" do
+        #pending "Отключено на время отладки что бы не захламлять базу ненужными юзерами."
         find(:xpath, ".//input[@name='commit']").click
         
         #FIXME: пока костыль... после исправления бага нужно переделать.
@@ -42,11 +48,118 @@ describe "User pages" do
       end
 
       it "should not create a user with the same email" do
+        #pending "Отключено на время отладки что бы не захламлять базу ненужными юзерами."
         find(:xpath, ".//input[@name='commit']").click
 
         should have_selector(:xpath, ".//div[contains(@class,'user_email') and contains(@class,'error')]")
       end
 
+      it "should not create a user without process personal data conformation" do
+        find(:xpath, ".//input[@name='user[process_personal_data]']").click
+        find(:xpath, ".//input[@name='commit']").click
+    
+        should have_selector(:xpath, ".//div[contains(@class,'user_process_personal_data') and contains(@class,'error')]")
+      end
+
     end
+
+    describe "with invalid email" do
+        it "should change field color to red after form submit" do
+
+            fill_in "user[email]", with: "invalid_email"
+            find(:xpath, ".//input[@name='commit']").click
+            
+            should have_selector(:xpath, ".//div[contains(@class,'user_email') and contains(@class,'error')]")
+            should have_selector(:xpath, ".//div[contains(@class,'user_email') and contains(@class,'error')]/div/span[@class='help-inline']")
+        end
+    end
+
+    describe "with empty password" do
+        it "should change field color to red after form submit" do
+
+            fill_in "user[password]",                  with: ""
+            find(:xpath, ".//input[@name='commit']").click
+            
+            should have_selector(:xpath, ".//div[contains(@class,'user_password') and contains(@class,'error')]")
+            should have_selector(:xpath, ".//div[contains(@class,'user_password') and contains(@class,'error')]/div/span[@class='help-inline']")
+        end
+    end
+
+    describe "without password confirmation" do
+        it "should change field color to red after form submit" do
+
+            fill_in "user[password]",                  with: "123"
+            find(:xpath, ".//input[@name='commit']").click
+            
+            should have_selector(:xpath, ".//div[contains(@class,'user_password') and contains(@class,'error')]")
+            should have_selector(:xpath, ".//div[contains(@class,'user_password') and contains(@class,'error')]/div/span[@class='help-inline']")
+        end
+    end
+
+    describe "with invalid user first name" do
+        it "should change field color to red after form submit" do
+
+            fill_in "user[first_name]",                  with: "123~!@#\$\%^&*()_+|\":;'"
+            find(:xpath, ".//input[@name='commit']").click
+            
+            should have_selector(:xpath, ".//div[contains(@class,'user_first_name') and contains(@class,'error')]")
+            should have_selector(:xpath, ".//div[contains(@class,'user_first_name') and contains(@class,'error')]/div/span[@class='help-inline']")
+        end
+    end
+
+    describe "with invalid user last name" do
+        it "should change field color to red after form submit" do
+
+            fill_in "user[last_name]",                  with: "123~!@#\$\%^&*()_+|\":;'"
+            find(:xpath, ".//input[@name='commit']").click
+            
+            should have_selector(:xpath, ".//div[contains(@class,'user_last_name') and contains(@class,'error')]")
+            should have_selector(:xpath, ".//div[contains(@class,'user_last_name') and contains(@class,'error')]/div/span[@class='help-inline']")
+        end
+    end
+
+    describe "with invalid user city" do
+        it "should change field color to red after form submit" do
+
+            fill_in "user[city]",                  with: "123~!@#\$\%^&*()_+|\":;'"
+            find(:xpath, ".//input[@name='commit']").click
+            
+            should have_selector(:xpath, ".//div[contains(@class,'user_city') and contains(@class,'error')]")
+            should have_selector(:xpath, ".//div[contains(@class,'user_city') and contains(@class,'error')]/div/span[@class='help-inline']")
+        end
+    end
+
+    describe "with input user city" do
+        it "should show autocomplite popup with founded cities" do
+
+            fill_in "user[city]",                  with: "новос"
+            sleep 1
+
+            should have_selector(:xpath, ".//ul[@id='ui-id-2']/li/a[text()='новосибирск']")
+
+            fill_in "user[city]",                  with: "новоу"
+            sleep 1
+
+            should_not have_selector(:xpath, ".//ul[@id='ui-id-2']/li/a[text()='новосибирск']")
+            should have_selector(:xpath, ".//ul[@id='ui-id-2']/li/a[text()='новоульяновск']")
+        end
+    end
+
+    describe "with input user company" do
+        it "should show autocomplite popup with founded companies" do
+
+            fill_in "user[company]",                  with: "simb"
+            sleep 1
+
+            should have_selector(:xpath, ".//ul[@id='ui-id-1']/li/a[text()='SimbirSoft']")
+
+            fill_in "user[company]",                  with: "simt"
+            sleep 1
+
+            should_not have_selector(:xpath, ".//ul[@id='ui-id-1']/li/a[text()='SimbirSoft']")
+            should have_selector(:xpath, ".//ul[@id='ui-id-1']/li/a[text()='Simtech']")
+        end
+    end
+
   end
 end
